b950444e8da4e17066d98bcd9043b07125973d5c
Konstantin Boryagin <nokss89@gmail.com>
2021-08-03 23:17:59 +0300

    add folder test.java

---
+++ src/test/java/ru/otus/QuizServiceTest.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+import junit.framework.TestCase;
+
+public class QuizServiceTest extends TestCase {
+
+    public void testResultOfGame() {
+    }
+
+    public void testSetPlayerName() {
+    }
+}
