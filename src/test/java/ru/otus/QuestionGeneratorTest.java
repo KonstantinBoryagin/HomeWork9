889a38ee717142356000edb21f45ea893911e340
Konstantin Boryagin <nokss89@gmail.com>
2021-08-07 16:42:56 +0300

    add prinQuestionTest method

--- src/test/java/ru/otus/QuestionGeneratorTest.java
+++ src/test/java/ru/otus/QuestionGeneratorTest.java
@@ -1,20 +1,44 @@
 package ru.otus;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-public class QuestionGeneratorTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionGeneratorTest {
 
     @Test
-    public void formArrayOfQuestions() {
+    void formArrayOfQuestions() {
     }
 
     @Test
     public void printQuestion() {
+        String consoleOutput = "";
+        PrintStream originOut = System.out;
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(100);
+            PrintStream capture = new PrintStream(outputStream);
+            System.setOut(capture);
+            QuestionGenerator temp = new QuestionGenerator();
+            Question test = Question.QUESTION_ONE;
+            temp.printQuestion(test);
+            capture.flush();
+            consoleOutput = outputStream.toString();
+            System.setOut(originOut);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals("\nСколько примитивных типов данных в Java?\n" +
+                "  1. Десять                2. Пять                \n" +
+                "  3. Восемь              \n" +
+                " Введите номер ответа - ", consoleOutput);
     }
 
+
     @Test
-    public void getCountOfQuestions() {
+    void getCountOfQuestions() {
     }
 }
